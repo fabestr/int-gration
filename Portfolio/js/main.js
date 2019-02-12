2794a660cdb2047652205fe8e0066255f5a06b1b
fabestr <f.estrabaud@gmail.com>
2019-02-12 14:16:50 +0100

    portfolio title

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -2,8 +2,11 @@
 
 var menuHidde = document.getElementById('menu_hidde');
 var menuClick = document.querySelector('.menu_click');
+var position = "name"
+
 
 menuClick.addEventListener('click', onClickToggleMenu);
+
 
 function onClickToggleMenu(event) {
     event.preventDefault();
@@ -11,6 +14,31 @@ function onClickToggleMenu(event) {
     menuHidde.classList.toggle('slide_menu');
 
     menuHidde.classList.toggle('slide_menu_return');
+    setTimeout(function(){
+        menuHidde.classList.toggle('hidden'); 
+    }, 500);
 
-    menuHidde.classList.toggle('hidden');
 }
+
+$(window).scroll(function(){
+    
+      console.log($(window).scrollTop() );
+      if($(window).scrollTop() < 250 && position != "name") {
+        
+        console.log('name');
+        $('.title_main').toggleClass('anime_title');
+        $('.title_main').html('<span class="name">Fabien</span><span class="name">Estrabaud</span>');
+        //$('.title_main').toggleClass('anime_title');
+
+        position = "name";
+    
+    }
+      if($(window).scrollTop() > 250 && position != "competence") {
+        console.log('competence');
+            $('.title_main').toggleClass('anime_title');
+            $('.title_main').html('<span class="name">compétences</span>');
+           // $('.title_main').toggleClass('anime_title');
+            position = "competence";
+      }
+    
+  });
--- Portfolio/login.php
+++ Portfolio/login.php
@@ -1,7 +1,40 @@
 <?php
+session_start();
+
+if(empty($_POST)==false) {
+    //var_dump($_POST);
+
+    $pdo = new PDO('mysql:host=localhost;dbname=portfolio', 'root', 'troiswa');
+
+	$pdo->exec('SET NAMES UTF8');
+    
+    $query = $pdo->prepare
+	(
+	    'SELECT * FROM users WHERE email= ? AND psw=?'
+	);
+
+	$query->execute( [ $_POST['email'],$_POST['psw'] ] );
+    
+    $user = $query->fetch(PDO::FETCH_ASSOC);
+
+	if( $user['email'] == $_POST['email'] && $user['psw'] == $_POST['psw'] ) {
+    
+    	//var_dump('connecté');
+        
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['psw'] = $user['psw'];
+        $_SESSION['firstName'] = $user['firstName'];
+        $_SESSION['lastName'] = $user['lastName'];
+        $_SESSION['pseudo'] = $user['pseudo'];
+        
+        //var_dump($_SESSION);
+
+        header('Location: index.php');
+        exit();
+    }
 
 
-
+}
 
 
 
