c5db507d35c630652126ce224a41eb9f6f2e6199
fabestr <f.estrabaud@gmail.com>
2019-02-13 17:36:00 +0100

    cest de la merde

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,8 +1,22 @@
 'use strict';
 
+
+/* $(window).load(function(event) {
+    event.preventDefault();
+    $(".loader").fadeOut();
+    $('.container').fadeIn();
+}) */
+
+/* 
+document.addEventListener('DOMContentLoaded', function(event) {
+    // @link  https://developer.mozilla.org/fr/docs/Web/Events/DOMContentLoaded
+   
+    document.getElementsByClassName('loader')[0].style.display = 'none';
+  }); */
+
 var menuHidde = document.getElementById('menu_hidde');
 var menuClick = document.querySelector('.menu_click');
-var position = "name"
+var position = "name";
 
 
 menuClick.addEventListener('click', onClickToggleMenu);
@@ -25,20 +39,22 @@ $(window).scroll(function(){
       console.log($(window).scrollTop() );
       if($(window).scrollTop() < 250 && position != "name") {
         
-        console.log('name');
-        $('.title_main').toggleClass('anime_title');
+        //$('.title_main').toggleClass('anime_title');
         $('.title_main').html('<span class="name">Fabien</span><span class="name">Estrabaud</span>');
         //$('.title_main').toggleClass('anime_title');
-
-        position = "name";
+       position = "name";
     
     }
-      if($(window).scrollTop() > 250 && position != "competence") {
-        console.log('competence');
+      if($(window).scrollTop() > 250 && position != "formation") {
+        console.log('formation');
+            //$('.title_main').toggleClass('anime_title');
+            $('.title_main').html('<span class="name">Formation</span>');
             $('.title_main').toggleClass('anime_title');
-            $('.title_main').html('<span class="name">compétences</span>');
-           // $('.title_main').toggleClass('anime_title');
-            position = "competence";
+            position = "formation";
       }
     
   });
+
+
+
+  
